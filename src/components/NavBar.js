d6b2eca5d7e716ec0318afa70320a5acca84696c
Monir Hossen <monirhossen@pursuit.org>
2023-11-19 18:19:01 -0500

    NabBar re-designed according to the wireframe

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,11 +9,17 @@ export default function NavBar() {
 </Link>
 <nav>
 <ul className="nav_links">
+<li> <a href ="/articles"> Articles</a> </li>
+<li> <a href ="/about"> About </a> </li>
 <li> <a href ="/mission"> Mission</a> </li>
-<li> <Link to ="/about"> About </Link> </li>
+<li> <a href ="/contact"> Contact</a></li>
+<li style ={{float:"right"}}> <a  href="/login"> Login</a> </li>
+<li style ={{float:"right"}}> <a href ="/signup"> Sign Up</a> </li>
 </ul>
+
+
 </nav>
-<a className="button" href="/contact"><button> Contact</button></a>
+{/* <a className="button" href="/contact"><button> Contact</button></a> */}
 </div>
   );
 }
