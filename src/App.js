aa31ed14222d6dba2f572b5922714a602684603b
Raymond <raymondjohnson@pursuit.org>
2023-11-26 06:32:20 -0500

    added profile component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/posts" element={<Forum />} />
+            <Route path="/forum" element={<Forum />} />
           </Routes>
         </main>
       </Router>
