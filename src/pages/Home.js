f568e0a8d0ec479eadabab846a8b1a0740c2d38d
Your Name <you@example.com>
2024-01-17 20:22:25 -0500

    homepage & logo changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
 
 
-// import { Link } from "react-router-dom";
-import logo from '../assets/logo.png';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import './Home.css';
