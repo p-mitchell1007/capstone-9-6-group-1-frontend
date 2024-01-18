896f7ff0a0e41849c12c99fdb91cf3a4ceb039b6
winterbelle <blancaaltamirano@pursuit.org>
2024-01-17 21:29:12 -0500

    Changes to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
-
-
+import { Link } from "react-router-dom";
+import logo from '../assets/logo.png';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import YoutubeEmbed from '../components/PPDVideo';
+
+import PPD2 from '../assets/PPD 2.png'
 import './Home.css';
 
 const randomQuotesAPI = "https://api.quotable.io/random";
@@ -35,8 +38,8 @@ const fetchArticles = async () => {
       },
     });
 
-    const articleData = response.data.articles.map((article) => ({
-      title: article.title,
+    const articleData = response.data.articles.slice(0,3).map((article) => ({
+      title: article.title.slice(0, 120) + '...',
       description: article.description,
       url: article.url,
     }));
@@ -51,6 +54,11 @@ const fetchArticles = async () => {
 
 return (
   <div className="home">
+    <div className="CTA">
+      <h3>Welcome to Reflections!</h3>
+      <p>Feeling overwhelmed postpartum? Need to talk to others  who are going 
+        through the same thing? You are not alone! <span><a href="/forum">Join the conversation</a></span>.</p>
+    </div>
     <div className="content">
       {/* Random Quote Section */}
       <div className="random-quote">
@@ -63,23 +71,21 @@ return (
       {/* Articles Section */}
       <div className="related-articles">
         <h2>
-          Trending Articles <span>on Postpartum Depression</span>
+          Trending Articles <span>on Reflections</span>
         </h2>
         <div className="article-grid">
           {articles.slice(0, 4).map((article) => (
-            <div key={article.id} className="article">
-              {/* Logo */}
-              <img src="logo.png" alt="Your Logo" className="logo" />
-              <div className="article-content">
-                <h3>{article.title}</h3>
+            <div  key={article.id} className="article">
+              <img className="article__img" src={PPD2} />
+                <h4>{article.title}</h4>
                 <p>{article.body}</p>
                 <p>
                   Read More
                 </p>
-              </div>
-              <hr />
             </div>
           ))}
+          {/* <iframe width="518" height="365" src="https://www.youtube.com/embed/KsnY-fSfNlU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+          <YoutubeEmbed embedId='KsnY-fSfNlU' />
         </div>
       </div>
     </div>
